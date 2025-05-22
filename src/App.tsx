0efcd86a788c78028ec6e96689250208f625f5f5
Kushan Nalinka <kushannalinka572@gmail.com>
2025-05-22 17:19:45 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -27,12 +27,12 @@ import CandidateCharts from "./pages/CandidateCharts";
 import SingleCandidate from "./pages/SingleCandidate";
 import SingleFinalized from "./pages/SingleFinalized";
 
+import CompletedCandidates from "./pages/CompletedCandidates";
 
-// import RankedCandidates from "./pages/RankedCandidates";
+import RankedCandidates from "./pages/RankedCandidates";
 // import GitHubScreen from "./pages/GitHubScreen";
 // import LinkedinScreen from "./pages/LinkedinScreen";
 // import TranscriptScreen from "./pages/TranscriptScreen";
-// import CompletedCandidates from "./pages/CompletedCandidates";
 
 
 
@@ -58,9 +58,9 @@ export default function App() {
             <Route path="/candidate-charts/:candidateID" element={<CandidateCharts />} />
             <Route path="/single/candidate/:id" element={<SingleCandidate />} />
             <Route path="/single/finalized/:id" element={<SingleFinalized />} />
-            {/* <Route path="/finalized/ranked/:jobId" element={<RankedCandidates />} />
+            <Route path="/finalized/ranked/:jobId" element={<RankedCandidates />} />
             <Route path="/completed/final-ranked/:jobId" element={<CompletedCandidates />} />
-            <Route path="/single/github/:id" element={<GitHubScreen />} />
+            {/* <Route path="/single/github/:id" element={<GitHubScreen />} />
             <Route path="/single/linkedin/:id" element={<LinkedinScreen/>} />
             <Route path="/single/transcript/:id" element={<TranscriptScreen />} /> */}
 
